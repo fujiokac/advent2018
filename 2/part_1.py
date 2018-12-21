35f90c71de76bcb89aa7ab75e819c1bb520ecbc6
Chie Fujioka <fujioka.chie@gmail.com>
2018-12-21 04:32:16 +0000

    Removed reinvented wheel in Part 1. PFF Python SO CONVENIENT!

--- 2/part_1.py
+++ 2/part_1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def readFile():
     try:
         with open('input.txt','r') as file:
@@ -5,30 +7,14 @@ def readFile():
     finally:
         file.close()
 
-def parseId(boxId):
-    dct = {}
-    for letter in boxId:
-        if letter in dct:
-            dct[letter] += 1
-        else:
-            dct[letter] = 1
-    return dct
-
-def containsNum(parsedId, num):
-    for letter in parsedId:
-        if parsedId[letter] == num:
-            return True
-    return False
-
 def checksum():
     twos = 0
     threes = 0
-    parsedIds = [parseId(boxId) for boxId in readFile()]
-    for pid in parsedIds:
-        if containsNum(pid, 2):
-            twos += 1
-        if containsNum(pid, 3):
-            threes += 1
+    for boxId in readFile():
+        counts = Counter(boxId).values()
+        twos += 2 in counts
+        threes += 3 in counts
+
     return twos * threes
 
 print(checksum())
