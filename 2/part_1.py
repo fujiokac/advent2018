9ea01c31d57d21511f0c964998bbb3f99fcfde9e
Chie Fujioka <fujioka.chie@gmail.com>
2018-12-21 03:30:53 +0000

    Day 2 Part 1

---
+++ 2/part_1.py
@@ -0,0 +1,34 @@
+def readFile():
+    try:
+        with open('input.txt','r') as file:
+            return [line for line in file]
+    finally:
+        file.close()
+
+def parseId(boxId):
+    dct = {}
+    for letter in boxId:
+        if letter in dct:
+            dct[letter] += 1
+        else:
+            dct[letter] = 1
+    return dct
+
+def containsNum(parsedId, num):
+    for letter in parsedId:
+        if parsedId[letter] == num:
+            return True
+    return False
+
+def checksum():
+    twos = 0
+    threes = 0
+    parsedIds = [parseId(boxId) for boxId in readFile()]
+    for pid in parsedIds:
+        if containsNum(pid, 2):
+            twos += 1
+        if containsNum(pid, 3):
+            threes += 1
+    return twos * threes
+
+print(checksum())
