d36be7433dc43600c0dfb4eb6981fa2d910c64ec
Chie Fujioka <fujioka.chie@gmail.com>
2018-12-21 15:37:35 +0000

    Thought of logical bug (even though previous version worked). Overhauled algorithm, trying to avoid nested loop because reasons.

--- 2/part_2.py
+++ 2/part_2.py
@@ -1,5 +1,3 @@
-import itertools as it
-
 def readFile():
     try:
         with open('input.txt','r') as file:
@@ -7,31 +5,41 @@ def readFile():
     finally:
         file.close()
 
-def pairwise(iterable):
-    #itertools recipe
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = it.tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
-def diffExpected(id1, id2, expected = 1):
-    diff = 0
-    for a, b in zip(id1, id2):
-        if a != b:
-            if diff < expected:
-                diff += 1
-            else:
-                return False
-    return diff == expected
-
-def commonLetters(id1, id2):
-    return ''.join([a for a, b in zip(id1, id2) if a == b])
-
 def findCommonId():
     ids = readFile()
-    for id1, id2 in pairwise(sorted(ids)):
-        if diffExpected(id1, id2):
-            return commonLetters(id1, id2)
-    return None
+    trie = {}
+    for boxId in ids:
+        match = matchId(boxId, trie, False)
+        if match is not None:
+            return match
+
+def matchId(boxId, trie, different):
+    '''
+        Parse IDs adding letters to trie
+        Branches into alternate matching on first difference
+        Otherwise returns None
+    '''
+    if len(boxId) == 0:
+        return None if different else ""
+    letter = boxId[0]
+    if letter not in trie:
+        # boxId is different from previous on this letter
+        if different is None:
+            # Matching search, no difference tolerated
+            return None
+        if not different:
+            # No previous differences
+            # Searching through other letters for a match with 0 tolerance
+            for otherLetter in trie:
+                match = matchId(boxId[1:], trie[otherLetter], None)
+                if match is not None:
+                    return match
+            different = True
+        # Match not found, continuing to build data
+        trie[letter] = {}
+    # Continue matching/building data recursively
+    match = matchId(boxId[1:], trie[letter], different)
+    return None if match is None else letter + match
+
 
 print(findCommonId())
