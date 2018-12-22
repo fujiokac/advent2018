45152b4ae16dd29fdff8a0771643905fa7b6e3f0
Chie Fujioka <fujioka.chie@gmail.com>
2018-12-22 20:25:39 +0000

    Modularizing to prepare for part 2

--- 5/part_1.py
+++ 5/part_1.py
@@ -3,25 +3,24 @@ import string
 
 FILE = 'input.txt'
 
-def parse_as_read():
-    polymer = deque('')
+def read_file():
     try:
         with open(FILE,'r') as file:
-            unit = file.read(1)
-            while(unit):
-                scan_polymer(polymer, unit)
-                unit = file.read(1)
+            return file.read().splitlines()
     finally:
         file.close()
 
+def react_polymer(raw):
+    polymer = deque('')
+    for unit in raw:
+        scan(polymer, unit)
+
     return ''.join(polymer)
 
-def scan_polymer(polymer, unit):
-    if unit not in string.ascii_letters:
-        return
+def scan(polymer, unit):
     if len(polymer) > 0 and polymer[-1] == unit.swapcase():
         polymer.pop()
     else:
         polymer.append(unit)
 
-print(len(parse_as_read()))
+print(len(react_polymer(read_file()[0])))
