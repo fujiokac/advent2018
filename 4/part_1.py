2eeb86942cb62b5567cb48c68f73ea17b9a097d7
Chie Fujioka <fujioka.chie@gmail.com>
2018-12-22 05:08:31 +0000

    D4P2 Complete. Two-fer-one special! Strategy 2 could probably be optimized. Changed Strategy 1 from mode JBC.

--- 4/part_1.py
+++ 4/part_1.py
@@ -1,8 +1,11 @@
 from statistics import mode
+from collections import Counter
 import re
 
 MINUTE = re.compile(r':(\d{2})\]')
 GUARD = re.compile(r'#(\d+)')
+TOTAL = 'total'
+MINUTES = 'minutes'
 
 def read_file():
     try:
@@ -23,18 +26,26 @@ def parse_log(log):
             awake = minute
             if id not in guards:
                 guards[id] = {}
-                guards[id]['total'] = 0
-                guards[id]['minutes'] = []
-            guards[id]['total'] += awake - asleep
-            guards[id]['minutes'].extend(range(asleep,awake))
+                guards[id][TOTAL] = 0
+                guards[id][MINUTES] = []
+            guards[id][TOTAL] += awake - asleep
+            guards[id][MINUTES].extend(range(asleep,awake))
     return guards
 
 def sleepiest_guard(guards):
-    return max(guards, key=lambda key: guards[key]['total'])
+    return max(guards, key=lambda key: guards[key][TOTAL])
 
 def strategy_1(id, minutes):
-    return id * mode(minutes)
+    return id * Counter(minutes).most_common(1)[0][0]
+
+def strategy_2(guards):
+    id = max(guards, key=lambda key: Counter(guards[key][MINUTES]).most_common(1)[0][1])
+    minute = Counter(guards[id][MINUTES]).most_common(1)[0][0]
+    return id, minute
+
 
 guards = parse_log(sorted(read_file()))
 id = sleepiest_guard(guards)
-print(id, guards[id]['total'], strategy_1(id, guards[id]['minutes']))
+print("strategy 1 -- id:{} total:{} answer:{}".format(id, guards[id][TOTAL], strategy_1(id, guards[id][MINUTES])))
+id, minute = strategy_2(guards)
+print("strategy 2 -- id:{} minute:{} answer:{}".format(id, minute, id * minute))
