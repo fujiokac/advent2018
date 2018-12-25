df94f1bac4d6c977eb57e792deec896e9239a789
Chie Fujioka <fujioka.chie@gmail.com>
2018-12-25 22:13:23 +0000

    Just documenting part 2

--- 7/main.py
+++ 7/main.py
@@ -47,21 +47,27 @@ def part2(prereqs):
 	time = 0
 	elves = {}
 	while(True):
+		# Check for completed tasks
 		complete = [elf for elf in elves if elves[elf] <= time]
 		for task in complete:
 			elves.pop(task)
 		done += complete
 		todo -= set(complete)
 		working_elves = len(elves)
+		# End loop if all planned and ongoing tasks completed
 		if(len(todo) == 0 and working_elves == 0):
 			break
+		# If any workers available, check for available work
 		if working_elves < WORKERS:
 			completed = set(done)
+			# In case there is more work than workers
 			for times in range(working_elves, WORKERS):
 				step = get_next_step(prereqs, todo - set(elves.keys()), completed)
 				if step is not None:
+					# Add available work with time of completion
 					elves[step] = time + time_per_step(step)
 				else:
+					# Nothing available right now
 					break
 		time += 1
 	return ''.join(done), time
